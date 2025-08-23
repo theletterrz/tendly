c78dbb7ee19461939f3fe66ff12927e734bcb4eb
alsoknownaszac <mayo16collins@gmail.com>
2025-08-23 15:33:40 +0100

    abstraXION inetgration: wrapping app with abstraxion provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,74 @@
+import 'react-native-reanimated';
+import 'react-native-get-random-values';
+import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import { Platform } from 'react-native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider as NavigationThemeProvider,
+} from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { Platform, View } from 'react-native';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
-import { View } from 'react-native';
+
+import { AbstraxionProvider } from '@burnt-labs/abstraxion-react-native';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { ThemeProvider } from '@/contexts/ThemeContext';
+
+import { Buffer } from 'buffer';
+import crypto from 'react-native-quick-crypto';
+global.crypto = crypto;
+global.Buffer = Buffer;
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+const treasuryConfig = {
+  treasury: process.env.EXPO_PUBLIC_TREASURY_CONTRACT_ADDRESS, // Example XION treasury instance
+  gasPrice: '0.001uxion', // If you feel the need to change the gasPrice when connecting to signer, set this value. Please stick to the string format seen in example
+  rpcUrl: process.env.EXPO_PUBLIC_RPC_ENDPOINT,
+  restUrl: process.env.EXPO_PUBLIC_REST_ENDPOINT,
+  callbackUrl: 'tendly://', // this comes from app.json scheme
+};
 
 export default function RootLayout() {
-  useFrameworkReady();
+  const systemColorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
-    <View
-      style={{
-        width: Platform.OS === 'web' ? 460 : '100%',
-        height: '100%',
-        alignSelf: 'center',
-      }}
-    >
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <AbstraxionProvider config={treasuryConfig}>
+        <NavigationThemeProvider
+          value={systemColorScheme === 'dark' ? DarkTheme : DefaultTheme}
+        >
+          <View
+            style={{
+              width: Platform.OS === 'web' ? 460 : '100%',
+              height: '100%',
+              alignSelf: 'center',
+            }}
+          >
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </View>
+          <StatusBar style="auto" />
+        </NavigationThemeProvider>
+      </AbstraxionProvider>
+    </ThemeProvider>
   );
 }
